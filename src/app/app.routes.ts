f7fb9d2bef26d4cee4002124f000330527b3dd5e
fatehortua <francyatehortua@gmail.com>
2026-04-26 11:23:05 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,9 +10,6 @@ export const routes: Routes = [
   { path: 'servicios', component: ServiciosComponent },
   { path: 'servicios/:id', component: DetalleComponent },
   { path: 'favoritos', component: FavoritosComponent },
-<<<<<<< HEAD
   { path: 'contacto', component: ContactoComponent },
-=======
->>>>>>> 7881f349d69a98351e6d21b7d6216cf9bc243679
   { path: '**', redirectTo: '' }
 ];
